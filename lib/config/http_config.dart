536d0bcc7ce0900daf844673f70e733b2aebaea5
fefsouza10 <fefsouza10@gmail.com>
2023-02-10 00:45:19 -0300

    feature: creating realtime weather model and service, fetching realtime weather from WeatherAPI.com with user's current latitude and longitude.

---
+++ lib/config/http_config.dart
@@ -0,0 +1,6 @@
+abstract class HttpConfig {
+  final Map<String, String> headers = {
+    'X-RapidAPI-Key': 'c4346478b3msh6b40dc5dbd9cbf9p17b197jsna0f4da2a04c0',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  };
+}
